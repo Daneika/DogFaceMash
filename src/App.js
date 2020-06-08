62bc68f9157b08421b6d1848884a3c2d60bc5134
meg <megan.daneika@gmail.com>
2020-06-08 11:59:33 +0100

    Add AppBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { orange } from '@material-ui/core/colors';
+
+import Navbar from "./components/navbar"
+
+const customTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[500],
+    },
+  },
+});
+
 function App() {
   return (
-    <div>
-  
-    </div>
+    <ThemeProvider theme={customTheme}>
+  <Navbar/>
+    </ThemeProvider>
   );
 }
 
