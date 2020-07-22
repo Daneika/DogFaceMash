613ba205cadfcbb4fb4564e7f063c0b6754fdd94
meg <megan.daneika@gmail.com>
2020-07-22 17:03:12 +0100

    Add media query for smaller devices

--- src/components/home-page/picture-gallery/PictureGallery.js
+++ src/components/home-page/picture-gallery/PictureGallery.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import { makeStyles } from "@material-ui/core/styles";
@@ -16,19 +17,23 @@ const useStyles = makeStyles((theme) => ({
   gridList: {
     width: "50%",
     height: "50%",
-    overflow: "visible", 
+    overflow: "visible",
   },
 }));
 
 export default function PictureGallery() {
   const classes = useStyles();
-
+  const isSm = useMediaQuery((theme) => theme.breakpoints.up("md"));
   return (
     <div className={classes.container}>
-      <GridList className={classes.gridList} cols={4}>
+      <GridList className={classes.gridList} cols={isSm ? 4 : 2}>
         {Array.from({ length: 16 }).map((ignore, index) => (
-          <GridListTile className="tile"  key={`/images/Dog${index + 1}.jpg`} cols={1}>
-            <img 
+          <GridListTile
+            className="tile"
+            key={`/images/Dog${index + 1}.jpg`}
+            cols={1}
+          >
+            <img
               src={process.env.PUBLIC_URL + `/images/Dog${index + 1}.jpg`}
               alt={`Dog${index + 1}`}
             />
